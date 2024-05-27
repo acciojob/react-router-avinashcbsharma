046d51df29760d1d190ecfb121ada2001f48d2fd
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-05-27 17:10:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,10 +9,9 @@ const App = () => {
   return (
     <Routers>
         <Routes>
-            <Route path='/' element={<Layout/>}>
-                <Route index element={<Home/>} />
-                <Route path='/About' element={<About/>} />
-            </Route>
+           <Layout/>
+                <Route path='/' element={<Home/>} />
+                <Route path='/About' element={<About/>} />           
         </Routes>
     </Routers>   
   )
